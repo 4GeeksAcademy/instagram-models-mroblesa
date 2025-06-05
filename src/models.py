57865354bf9f5e673e8f3d41b2b319c5b649dd94
Maria Alejandra Robles <alejita-robles@hotmail.com>
2025-06-05 20:18:12 +0000

    finalizado

--- src/models.py
+++ src/models.py
@@ -11,88 +11,67 @@ class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     name: Mapped[str] = mapped_column(String(120), nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    user: Mapped[List["FavPlanet"]] = relationship(back_populates= 'user')
-    user: Mapped[List["FavPeople"]] = relationship(back_populates = 'user')
+    username: Mapped[str] = mapped_column(String(120), nullable=False)
+
+    author_id: Mapped[Comment]= relationship("Comment", back_populates= "author")
     
+
+
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
-            "name" : self.name
+            "name" : self.name,
+            "username": self.username
             # do not serialize the password, its a security breach
         }
 
-
-class Planets(db.Model):
+class Post(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(String(120), nullable=False)
-    description: Mapped[str] = mapped_column(String(250), nullable= False)
-    population: Mapped[int] = mapped_column(Integer, nullable=False)
-    gravity: Mapped[int] = mapped_column(Integer, nullable=False)
-    image: Mapped[str] = mapped_column(String(120), nullable=True)
-    planets: Mapped[List["People"]] = relationship(back_populates='birth_planet')
-    planet: Mapped["FavPlanet"] = relationship('planet')
-
-    def serialize(self):
-        return {
-            "id": self.id,
-            "name": self.name,
-            "description": self.description,
-            "population": self.population,
-            "gravity": self.gravity,
-            "image": self.image,
-            "planet_id": self.list
-        }
-
-class FavPlanet(db.Model):
-
-    id: Mapped[int] = mapped_column(primary_key=True, unique=True)
-    planet_id: Mapped[int] = mapped_column(ForeignKey("planets.id"), nullable=False)
+    title: Mapped[str] = mapped_column(String[80], nullable=False)
+    comment: Mapped[str] = mapped_column(String[400], nullable=False)
     user_id: Mapped[int] = mapped_column(ForeignKey("user.id"), nullable=False)
 
-    user: Mapped[User] = relationship(User, back_populates="fav_planet")
-    planet: Mapped["Planets"] = relationship("Planets")
+    post_id: Mapped[List[Media]] = relationship("Media", back_populates="post_media")
+    post_comment_id: Mapped[List[Comment]] = relationship("Comment", back_populates="post_comment")
 
     def serialize(self):
         return {
             "id": self.id,
-            "planet_id": self.planet_id,
-            "user_id": self.user_id
-       }
-
-class People(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(String(120), nullable=False)
-    age: Mapped[int] = mapped_column(Integer, nullable=False)
-    gender: Mapped[str] = mapped_column(String(120), nullable=False)
-    height: Mapped[str] = mapped_column(String(120), nullable=True)
-    image: Mapped[str] = mapped_column(String(120), nullable=True)
-    planet_id: Mapped[int] = mapped_column(ForeignKey("planets.id"))
-    birth_planet: Mapped["Planets"] = relationship("Planets", back_populates='planets')
-   
-    def serialize(self):
-        return {
-            "id": self.id,
-            "name": self.name,
-            "age": self.age,
-            "gender": self.gender,
-            "height": self.height,
-            "image": self.image,
-            "birth_planet": self.birth_planet
+            "title": self.title,
+            "comment": self.comment,
+            "user_id": self.list,
+            
         }
 
-class FavPeople(db.Model):
+class Media(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, unique=True)
-    people_id : Mapped[int] = mapped_column(ForeignKey("people.id"))
-    user_id : Mapped[int] = mapped_column(ForeignKey("user.id"))
+    url: Mapped[str] = mapped_column(String[300], nullable= False)
+    post_id: Mapped[int] = mapped_column(ForeignKey("post.id"), nullable=False)
 
-    user: Mapped["User"] = relationship("User", back_populates = 'fav_people')
-    people: Mapped["People"] = relationship("People")
+    post_media: Mapped[Post] = relationship(Post, back_populates="post_id")
 
-    def serialize_fav_people(self):
+
+    def serialize(self):
         return {
             "id": self.id,
-            "people_id" : self.people_id,
-            "user_id" : self.user_id
+            "url": self.url,
+            "post_id": self.post_id
+       }
+
+class Comment(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True, unique=True)
+    text: Mapped[str] = mapped_column(String[400], nullable=True)
+    author_id: Mapped[int] = mapped_column(ForeignKey("user.id"), nullable=False)
+    post_comment_id: Mapped[int] = mapped_column(ForeignKey("post.id"), nullable=False)
+
+    author: Mapped[User] = relationship("User", back_populates="author_id")
+    post_comment: Mapped[Post] = relationship("Post", back_populates="post_comment_id")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "text": self.text,
+            "author_id": self.author_id,
+            "post_comment_id": self.post_comment_id
         }
